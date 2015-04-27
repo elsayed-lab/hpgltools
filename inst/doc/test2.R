b4425d21d463d57b904df2c99c38148626cffaa1
Ashton Trey Belew <abelew@gmail.com>
2015-04-27 17:41:35 -0400

    made vignettes work, finally

---
+++ inst/doc/test2.R
@@ -0,0 +1,9 @@
+## ----setup---------------------------------------------------------------
+
+library(knitr)
+opts_knit$set(progress=TRUE, verbose=TRUE, error=TRUE,  fig.width=7, fig.height=7)
+
+print("TESTME")
+
+
+
