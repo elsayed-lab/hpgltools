3976a26ab1a0849cd27e0b809ca962c4a72b8828
Ashton Trey Belew <abelew@gmail.com>
2016-09-09 13:38:54 -0400

    splitting some tests into multiple pieces

---
+++ tests/testthat/test_15norm_batch.R
@@ -0,0 +1,103 @@
+library(testthat)
+library(hpgltools)
+context("Are normalizations consistent over time (Batch estimation/correction)?")
+
+## Note to self: Some recent changed to the creation of my expressionsets lead to changes in the order of the resulting data frames.
+## This is intended to make it easier for me to keep track of what is happening to the data by forcing it into a consistent order.
+## Sadly, this means that most of these tests fail because they assume the previous generic order of genes.  Thus I am now adding
+## the gene names to the tests.
+
+## This section is copy/pasted to all of these tests, that is dumb.
+datafile <- system.file("extdata/pasilla_gene_counts.tsv", package="pasilla")
+## Load the counts and drop super-low counts genes
+counts <- read.table(datafile, header=TRUE, row.names=1)
+counts <- counts[rowSums(counts) > ncol(counts),]
+## Set up a quick design to be used by cbcbSEQ and hpgltools
+design <- data.frame(row.names=colnames(counts),
+    condition=c("untreated","untreated","untreated",
+        "untreated","treated","treated","treated"),
+    libType=c("single_end","single_end","paired_end",
+        "paired_end","single_end","paired_end","paired_end"))
+metadata <- design
+colnames(metadata) <- c("condition", "batch")
+metadata[["sampleid"]] <- rownames(metadata)
+## Uses these genes for quick tests
+test_genes <- c("FBgn0000014","FBgn0000008","FBgn0000017","FBgn0000018", "FBgn0000024")
+## create_expt generates a .Rdata file which may be reread, do so.
+pasilla <- new.env()
+load("pasilla.Rdata", envir=pasilla)
+pasilla_expt <- pasilla[["expt"]]
+
+## Test batch
+expected <- c(3.333333, 64.500000, 3040.166667, 383.916667, 7.083333)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="limma"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("limma batch gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+## FIXME this is obviously broken
+expected <- c(1.60048774, 0.02101530, -0.07524254, 0.15555548, 0.49697157)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="limmaresid"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("limma-residuals batch gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+expected <- c(3.095141, 60.542865, 3032.546240, 355.354483, 6.666536)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="combatmod"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("combatmod from cbcbSEQ batch gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+expected <- c(4.875168, 87.749156, 4418.793105, 557.089237, 9.641209)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="sva"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("sva batch gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+## TODO Figure out what is up with these
+## pasilla_batch <- normalize_expt(pasilla_expt, batch="combat")  ## broken
+## pasilla_batch <- normalize_expt(pasilla_expt, batch="combat_noprior") ## takes forever
+expected <- c(2.267660, 71.646546, 3481.434409, 410.347808, 6.658058)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="combat_scale"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("combat_scale gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+## TODO Figure this guy out
+## pasilla_batch <- normalize_expt(pasilla_expt, batch="combat_noprior_scale") ## takes forever
+expected <- c(4.610009, 82.109047, 4099.039071, 519.407501, 9.116170)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="svaseq"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("svaseq gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
+
+## FIXME I broke ruvg
+## pasilla_batch <- normalize_expt(pasilla_expt, batch="ruvg") ## broken for the moment
+
+
+expected <- c(4.610009, 82.109047, 4099.039071, 519.407501, 9.116170)
+names(expected) <- test_genes
+pasilla_batch <- sm(normalize_expt(pasilla_expt, batch="varpart"))
+actual_df <- Biobase::exprs(pasilla_batch[["expressionset"]])
+actual <- actual_df[test_genes, c("untreated1")]
+test_that("svaseq gives expected values?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
---
+++ tests/testthat/test_18norm_surrogates.R
@@ -0,0 +1,62 @@
+library(testthat)
+library(hpgltools)
+library(pasilla)
+data(pasillaGenes)
+
+context("Do surrogate estimators provide expected outputs?")
+
+pasilla <- new.env()
+load("pasilla.Rdata", envir=pasilla)
+pasilla_expt <- pasilla[["expt"]]
+
+pasilla_svasup <- s_p(get_model_adjust(pasilla_expt, estimate_type="sva_supervised"))[["result"]]
+expected <- c(0.1184778, 0.5678904, 0.4166829, 0.2938793, 0.5250849, 0.2844593, 0.2164316)
+actual <- abs(as.numeric(pasilla_svasup[["model_adjust"]]))
+test_that("Have the sva supervised model adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.000001)
+})
+
+pasilla_svaunsup <- s_p(get_model_adjust(pasilla_expt, estimate_type="sva_unsupervised"))[["result"]]
+expected <- c(0.1185214, 0.5617694, 0.4119131, 0.2947645, 0.5325897, 0.2850143, 0.2211886)
+actual <- abs(as.numeric(pasilla_svaunsup[["model_adjust"]]))
+test_that("Have the sva unsupervised model adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.001)
+})
+
+pasilla_pca <- s_p(get_model_adjust(pasilla_expt, estimate_type="pca"))[["result"]]
+actual <- as.numeric(pasilla_pca[["model_adjust"]])
+expected <- c(-0.1089195, -0.5708557, 0.4132932, 0.2933180, -0.5271456, 0.2846236, 0.2156861)
+test_that("Have the pca model adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.001)
+})
+
+pasilla_ruvsup <- s_p(get_model_adjust(pasilla_expt, estimate_type="ruv_supervised"))[["result"]]
+actual <- as.numeric(pasilla_ruvsup[["model_adjust"]])
+expected <- c(-0.1085012, -0.5712475, 0.4130910, 0.2928087, -0.5269890, 0.2849590, 0.2158789)
+test_that("Have the pca model adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.001)
+})
+
+pasilla_ruvresid <- s_p(get_model_adjust(pasilla_expt, estimate_type="ruv_residuals"))[["result"]]
+actual <- as.numeric(pasilla_ruvresid[["model_adjust"]])
+expected <- c(-0.2029818, -0.3981149, 0.2824784, 0.2863400, -0.6407893, 0.3101006, 0.3629670)
+test_that("Have the ruv resid model adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.001)
+})
+
+pasilla_ruvemp <- s_p(get_model_adjust(pasilla_expt, estimate_type="ruv_empirical"))[["result"]]
+actual <- as.numeric(pasilla_ruvemp[["model_adjust"]])
+expected <- c(-0.1409532, -0.5704923, 0.3987880, 0.2900914, -0.5148203, 0.3007386, 0.2366479)
+test_that("Have the ruv resid empirical adjustments stayed the same?", {
+    expect_equal(expected, actual, tolerance=0.001)
+})
+
+pasilla_surrogates <- s_p(compare_surrogate_estimates(pasilla_expt))[["result"]]
+actual <- as.numeric(as.character(pasilla_surrogates[["pca_adjust"]][["model_adjust"]]))
+expected <- as.numeric(c("-0.108919503928491", "-0.570855719628606",
+                         "0.413293224481248", "0.293317999343559",
+                         "-0.527145619820086", "0.284623554215443",
+                         "0.215686065336933"))
+test_that("Does the compare_surrogate stuff work?", {
+    expect_equal(expected, actual, tolerance=0.0001)
+})
