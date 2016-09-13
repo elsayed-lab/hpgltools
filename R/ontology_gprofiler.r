331f5f01c54b6f3e3cfa08c0c544c7dda9c831af
Ashton Trey Belew <abelew@gmail.com>
2016-09-13 18:18:38 -0400

    gr

--- R/ontology_gprofiler.r
+++ R/ontology_gprofiler.r
@@ -445,7 +445,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["bpp_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(bp_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(bp_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(bp_data) + 2
 
@@ -457,7 +457,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["mfp_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(mf_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(mf_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(mf_data) + 2
 
@@ -469,7 +469,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["ccp_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(cc_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(cc_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(cc_data) + 2
         openxlsx::setColWidths(wb, sheet=sheet, cols=2:7, widths="auto")
@@ -488,7 +488,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["kegg_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(kegg_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(kegg_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(kegg_data) + 2
     } ## End checking KEGG data
@@ -507,7 +507,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["tf_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(tf_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(tf_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(tf_data) + 2
         openxlsx::setColWidths(wb, sheet=sheet, cols=2:7, widths="auto")
@@ -526,7 +526,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["reactome_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(react_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(react_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(react_data) + 2
         openxlsx::setColWidths(wb, sheet=sheet, cols=2:7, widths="auto")
@@ -546,7 +546,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["mi_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(mi_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(mi_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(mi_data) + 2
         openxlsx::setColWidths(wb, sheet=sheet, cols=2:7, widths="auto")
@@ -565,7 +565,7 @@ write_gprofiler_data <- function(gprofiler_result, wb=NULL, excel="excel/gprofil
         if (isTRUE(add_plots)) {
             a_plot <- gprofiler_result[["plots"]][["corum_plot_over"]]
             print(a_plot)
-            openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(corum_data) + 2, startRow=new_row, fileType="png", units="in")
+            ins <- try(openxlsx::insertPlot(wb, sheet, width=6, height=6, startCol=ncol(corum_data) + 2, startRow=new_row, fileType="png", units="in"))
         }
         new_row <- new_row + nrow(corum_data) + 2
         openxlsx::setColWidths(wb, sheet=sheet, cols=2:7, widths="auto")
