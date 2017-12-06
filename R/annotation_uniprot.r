68f87ec439e29afafe993f1b0d1fb0357f2e2669
Ashton Trey Belew <abelew@gmail.com>
2017-12-05 19:22:27 -0500

    added some comments to the uniprotter
    and a catchall for the field types for which I am not interested.

--- R/annotation_uniprot.r
+++ R/annotation_uniprot.r
@@ -1,5 +1,6 @@
 
 parse_uniprot_txt_table <- function(file) {
+  ## file <-  "uniprot_3AUP000001584.txt.gz"
   read_vec <- readr::read_lines(file, progress=TRUE)
   gene_num <- 0
   ## Vectors for those elements which will only have 1 answer
@@ -22,8 +23,12 @@ parse_uniprot_txt_table <- function(file) {
     if ((i %% 10000) == 0) {
       message(paste0("On line ", i))
     }
-    ## Start with the master ID:
-    ## Example:  "ID   3MGH_MYCTU              Reviewed;         203 AA."
+    ## Start by skipping field types that we will never use.
+    if (grepl(pattern="^(DT|OS|OC|OX|RN|RA|RT|RP|RX|CC|PE|KW|FT|SQ)\\s+", x=line)) {
+      next
+    }
+    ## The master ID:
+    ## Example: ID   3MGH_MYCTU              Reviewed;         203 AA.
     if (grepl(pattern="^ID\\s+", x=line)) {
       gene_num <- gene_num + 1
       ## Initialize the ith element of our various data structures.
@@ -45,6 +50,8 @@ parse_uniprot_txt_table <- function(file) {
       amino_acids[gene_num] <- material[4]
       next
     }
+    ## Now pull the primary uniprot accesstions
+    ## Example: AC   P9WJP7; L0TAC1; O33190; P65412;
     if (grepl(pattern="^AC\\s+", x=line)) {
       material <- gsub(pattern=";", replacement="", x=strsplit(x=line, split="\\s+")[[1]])
       primary_accessions[gene_num] <- material[2]
@@ -52,10 +59,19 @@ parse_uniprot_txt_table <- function(file) {
       uniprot_accessions[[gene_num]] <- possible_accessions
       next
     }
+    ## Get the record names if available
+    ## Example: DE   RecName: Full=Putative 3-methyladenine DNA glycosylase {ECO:0000255|HAMAP-Rule:MF_00527};
+    ##          DE            EC=3.2.2.- {ECO:0000255|HAMAP-Rule:MF_00527};
     if (grepl(pattern="DE\\s+RecName:", x=line)) {
       recnames[gene_num] <- gsub(pattern="^.*Full=(.*?);.*$", replacement="\\1", x=line)
       next
     }
+    ## The GN field has a few interesting pieces of information and I think makes the primary link
+    ## between uniprot and the IDs available at genbank, ensembl, microbesonline, etc.
+    ## We may find one or more of the above fields in the GN, so I should take into account
+    ## the various possible iterations.
+    ## Example: GN   Name=pgl; Synonyms=devB; OrderedLocusNames=Rv1445c;
+    ##          GN   ORFNames=MTCY493.09;
     if (grepl(pattern="^GN\\s+", x=line)) {
       pat <- "^GN\\s+.*OrderedLocusNames=(.*?);.*$"
       if (grepl(pattern=pat, x=line)) {
@@ -79,6 +95,10 @@ parse_uniprot_txt_table <- function(file) {
       }
       next
     }
+    ## The DR field contains mappings to many other databases
+    ## Sadly, it too is quite a mess
+    ## This stanza looks for EMBL IDs:
+    ## Example: DR   EMBL; AL123456; CCP44204.1; -; Genomic_DNA.
     if (grepl(pattern="^DR\\s+EMBL;", x=line)) {
       tmp_ids <- gsub(pattern="^.*EMBL; (.*?) \\-;.*$", replacement="\\1", x=line)
       tmp_ids <- strsplit(x=tmp_ids, split="\\s+")[[1]]
