3861d01c9636da76730cdf7714367bf06363befd
Ashton Trey Belew <abelew@gmail.com>
2022-07-21 10:54:27 -0400

    docstrings

--- R/network.R
+++ R/network.R
@@ -1,8 +1,38 @@
 ## Attempt to standardize some network-formation functions.
 
-network_from_matrix <- function(score_file, metadata = NULL, type = "distcor", simplify = TRUE,
+#' Given a matrix of scores (bit score, e-value, etc), create an adjacency graph.
+#'
+#' I am hoping to use this as the starting point for a generic network generator.
+#' In its current form it takes a matrix of pairwise scores and
+#' generates an adjacency graph of those scores.
+#'
+#' @param score_file tsv or matrix of scores with column and row names containing IDs.
+#' @param metadata Currently unused, but intended to provide a
+#'  starting point for annotating the resulting adjacency network.
+#'  When implemented, it should make use of the annotate_network()
+#'  functions which follow.
+#' @param type Currently I only know of networks which use
+#'  correlation, distance, and distcor matrices of the original
+#'  scores; but I suspect a cursory glance at the WGCNA documentation
+#'  will teach me that there are many more possibilities.
+#' @param simplify Return a simplified matrix without loops and
+#'  redundancies?
+#' @param mode Network type to create, I don't yet understand the implications
+#'  of changing this.
+#' @param weighted Add weights to the nodes?  I also don't yet
+#'  understand what happens when you mess with this.
+#' @param diag Include the matrix-diagonal nodes?  I do not know when
+#'  one would want these.
+#' @return igraph adjacency network.
+network_from_matrix <- function(scores, metadata = NULL, type = "distcor", simplify = TRUE,
                                 mode = "undirected", weighted = TRUE, diag = FALSE) {
-  scores <- as.matrix(read.table(score_file, header = TRUE, sep = "\t", row.names = 1))
+  if ("character" %in% class(scores)) {
+    scores <- as.matrix(read.table(score_file, header = TRUE, sep = "\t",
+                                   row.names = 1))
+  } else {
+    scores <- as.matrix(scores)
+  }
+
   input <- scores
   if (type == "cor") {
     message("Calculating correlation matrix.")
@@ -27,6 +57,18 @@ network_from_matrix <- function(score_file, metadata = NULL, type = "distcor", s
   return(initial)
 }
 
+#' Use grep to add a vector of annotations/colors to a network.
+#'
+#' The igraph syntaxes are a little clunky, but the set_attr()
+#' functions mostly make sense.
+#'
+#' @param network Input network
+#' @param names set of node-names to which to add annotations.
+#' @param color Color to attach to the added annotation.
+#' @param default Set a default annotation for this name to all nodes.
+#' @param annot_name Annotation name to attach to the nodes.
+#' @param annot_value and the associated value.
+#' @return a new network!
 annotate_network <- function(network, names, color = NULL, default = NULL,
                              annot_name = "type", annot_value = "high") {
   net_names <- igraph::V(network)$name
@@ -47,6 +89,17 @@ annotate_network <- function(network, names, color = NULL, default = NULL,
   return(network)
 }
 
+#' A version of annotate_network, but which uses a dataframe as input.
+#'
+#' The annotate_network() function uses a vector of values, this
+#' extends that logic to add every column of a dataframe.  I would
+#' like to make this function a little more fun vis a vis abilities to
+#' add colors and such.
+#'
+#' @param network input network.
+#' @param df input dataframe, columns are the new metadata, rows are
+#'  the node-strings to search on.
+#' @param default Set a default?
 annotate_network_df <- function(network, df, default = NULL) {
   new <- network
   net_names <- igraph::V(network)$name
@@ -95,3 +148,5 @@ prune_network <- function(network, min_weight = 0.4, min_connectivity = 1) {
   }
   return(pruned_vertices)
 }
+
+## EOF
